bf3f610f23974789e1eb59db78bf9c2cc02d2a42
Ashish G <1410657@my.brunel.ac.uk>
2018-03-11 19:46:13 +0000

    Update Game_1000.java

--- app/src/main/java/com/example/ashish/pcexp/Game_1000.java
+++ app/src/main/java/com/example/ashish/pcexp/Game_1000.java
@@ -17,7 +17,7 @@ public class Game_1000 extends AppCompatActivity implements View.OnClickListener
 
     TextView cpu, mobo, gpu, ram, ssd, hdd, psu;
 
-    final int PROCESSOR_SELECTION_CODE = 0;
+//     final int PROCESSOR_SELECTION_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +47,7 @@ public class Game_1000 extends AppCompatActivity implements View.OnClickListener
 
 
         cpu = (TextView) findViewById(R.id.proNameG1000);
-        cpu.setText(getIntent().getStringExtra("ProcessorSelection"));
+        cpu.setText(getIntent().getStringExtra("Processor"));
 
         mobo = (TextView) findViewById(R.id.moboNameG1000);
         mobo.setText(getIntent().getStringExtra("Motherboard"));
@@ -69,21 +69,21 @@ public class Game_1000 extends AppCompatActivity implements View.OnClickListener
 
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+//     @Override
+//     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//         super.onActivityResult(requestCode, resultCode, data);
 
-        switch(requestCode) {
-            case (PROCESSOR_SELECTION_CODE) : {
-                if (resultCode == Activity.RESULT_OK) {
-                    // TODO Extract the data returned from the child Activity.
-                    String radioValue = data.getStringExtra("ProcessorSelection");
-                    // save the result value and update UI
-                }
-                break;
-            }
-        }
-    }
+//         switch(requestCode) {
+//             case (PROCESSOR_SELECTION_CODE) : {
+//                 if (resultCode == Activity.RESULT_OK) {
+//                     // TODO Extract the data returned from the child Activity.
+//                     String radioValue = data.getStringExtra("ProcessorSelection");
+//                     // save the result value and update UI
+//                 }
+//                 break;
+//             }
+//         }
+//     }
 
     @Override
     public void onClick(View v) {
@@ -113,7 +113,8 @@ public class Game_1000 extends AppCompatActivity implements View.OnClickListener
 
     public void opencpu_g1000() {
         Intent intent = new Intent(this, cpu_g1000.class);
-        startActivityForResult(intent, PROCESSOR_SELECTION_CODE);
+//         startActivityForResult(intent, PROCESSOR_SELECTION_CODE);
+        startActivity(intent);
     }
 
     public void openmobo_g1000() {
